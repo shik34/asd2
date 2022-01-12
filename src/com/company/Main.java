4e6719fa662dc61418463dfe8ee3d23b4ff67c76
shik34 <shik34@yandex.ru>
2022-01-12 18:33:15 +0700

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.US);
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        System.out.println ( ( (x<=0) && (x*x+y*y<1) ) || x>0 && (x*x+y*y<1) && y>x  ?"YES":"NO");
+    }
+}
